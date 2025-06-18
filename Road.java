baaec3693bd408a35b817dcff60e6786a52378af
SyedaFatimaJafri <syedarukhsarfatimajafri@gmail.com>
2025-06-18 16:28:05 +0500

    Create Road.java

---
+++ Road.java
@@ -0,0 +1,12 @@
+package package1;
+
+class Road {
+    String destination;
+    int weight;
+
+    Road(String destination, int weight) {
+        this.destination = destination;
+        this.weight = weight;
+    }
+}
+
